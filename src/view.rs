7c024d3327c7cc04dc279befa7a2912e20bfa3cf
Marko Mikulicic <mmikulicic@gmail.com>
2023-12-20 15:40:08 +0100

    Add RefCell import for testing in view.rs.

--- src/view.rs
+++ src/view.rs
@@ -20,20 +20,20 @@ pub struct ViewCmd {
 }
 
 trait Spanlike {
-    type SpanID;
+    type ID;
 
-    fn span_id(&self) -> Self::SpanID;
-    fn parent_span_id(&self) -> Self::SpanID;
+    fn id(&self) -> Self::ID;
+    fn parent_id(&self) -> Self::ID;
 }
 
 impl Spanlike for Span {
-    type SpanID = Vec<u8>;
+    type ID = Vec<u8>;
 
-    fn span_id(&self) -> Self::SpanID {
+    fn id(&self) -> Self::ID {
         self.span_id.clone().clone()
     }
 
-    fn parent_span_id(&self) -> Self::SpanID {
+    fn parent_id(&self) -> Self::ID {
         self.parent_span_id.clone()
     }
 }
@@ -47,25 +47,46 @@ where
     children: Rc<Cell<Vec<Node<S>>>>,
 }
 
+impl<S> std::fmt::Debug for Node<S>
+where
+    S: Spanlike + std::fmt::Debug,
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("Node")
+            .field("span", &self.span)
+            .field("children", &self.children.clone().take())
+            .finish()
+    }
+}
+
+impl<S> PartialEq for Node<S>
+where
+    S: Spanlike + PartialEq,
+{
+    fn eq(&self, other: &Self) -> bool {
+        self.span == other.span && self.children.clone().take() == other.children.clone().take()
+    }
+}
+
 fn build_tree<S>(spans: &[S]) -> Result<Vec<Node<S>>>
 where
     S: Spanlike + Clone + Default,
-    <S as Spanlike>::SpanID: Eq + Hash + Clone,
+    <S as Spanlike>::ID: Eq + Hash + Clone,
 {
     let mut children_map = HashMap::new();
 
     for span in spans {
         let children = Default::default();
-        children_map.insert(span.span_id().clone(), Rc::new(Cell::new(children)));
+        children_map.insert(span.id().clone(), Rc::new(Cell::new(children)));
     }
 
     for span in spans {
         let child = Node {
             span: span.clone(),
-            children: children_map.get(&span.span_id()).unwrap().clone(),
+            children: children_map.get(&span.id()).unwrap().clone(),
         };
 
-        if let Some(parent_cell) = children_map.get(&span.parent_span_id()) {
+        if let Some(parent_cell) = children_map.get(&span.parent_id()) {
             let mut parent_children = parent_cell.clone().take();
             parent_children.push(child);
             parent_cell.set(parent_children);
@@ -75,10 +96,10 @@ where
     let mut root_nodes: Vec<Node<S>> = vec![];
 
     for span in spans {
-        if children_map.get(&span.parent_span_id()).is_none() {
+        if children_map.get(&span.parent_id()).is_none() {
             root_nodes.push(Node {
                 span: span.clone(),
-                children: children_map.get(&span.span_id()).unwrap().clone(),
+                children: children_map.get(&span.id()).unwrap().clone(),
             });
         }
     }
@@ -209,5 +230,106 @@ impl ViewCmd {
         renderer.render(&tree, 0);
 
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::{cell::Cell, rc::Rc};
+
+    use crate::view::Node;
+
+    use super::{build_tree, Spanlike};
+
+    #[derive(Default, Debug, Clone, PartialEq)]
+    struct TestSpan<'a> {
+        id: &'a str,
+        parent_id: &'a str,
+    }
+
+    impl<'a> Spanlike for TestSpan<'a> {
+        type ID = &'a str;
+
+        fn id(&self) -> Self::ID {
+            self.id
+        }
+
+        fn parent_id(&self) -> Self::ID {
+            self.parent_id
+        }
+    }
+
+    // impl<'a> std::fmt::Debug for Node<TestSpan<'a>> {
+    //     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    //         f.debug_struct("Node").field("span", &self.span).finish()
+    //     }
+    // }
+
+    #[test]
+    fn build_tree_test() {
+        let spans = vec![
+            TestSpan {
+                id: "root-1",
+                parent_id: "",
+            },
+            TestSpan {
+                id: "s1",
+                parent_id: "root-1",
+            },
+            TestSpan {
+                id: "s2",
+                parent_id: "root-1",
+            },
+            TestSpan {
+                id: "s1-1",
+                parent_id: "s1",
+            },
+            TestSpan {
+                id: "root-2",
+                parent_id: "missing",
+            },
+        ];
+        let want = vec![
+            Node {
+                span: TestSpan {
+                    id: "root-1",
+                    parent_id: "",
+                },
+                children: Rc::new(Cell::new(vec![
+                    Node {
+                        span: TestSpan {
+                            id: "s1",
+                            parent_id: "root-1",
+                        },
+                        children: Rc::new(Cell::new(vec![Node {
+                            span: TestSpan {
+                                id: "s1-1",
+                                parent_id: "s1",
+                            },
+                            children: Rc::new(Cell::new(vec![])),
+                        }])),
+                    },
+                    Node {
+                        span: TestSpan {
+                            id: "s2",
+                            parent_id: "root-1",
+                        },
+                        children: Rc::new(Cell::new(vec![])),
+                    },
+                ])),
+            },
+            Node {
+                span: TestSpan {
+                    id: "root-2",
+                    parent_id: "missing",
+                },
+                children: Rc::new(Cell::new(vec![])),
+            },
+        ];
+
+        let got = build_tree(&spans).expect("build tree");
+        println!("{:?}", got);
+        println!("{:?}", want);
+        assert_eq!(got, want);
     }
 }
